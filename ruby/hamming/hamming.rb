b561eea21671e9781509e2a01ddbf925b1c736ef
yo7 <bloom6445@gmail.com>
2017-11-25 15:06:20 +0900

    Hamming: throw => raise
    Because of Kotlin / Java...

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -6,7 +6,7 @@ end
 
 class Hamming
   def self.compute(first_strand, second_strand)
-    throw ArgumentError unless first_strand.length == second_strand.length
+    raise ArgumentError.new("Two strands must be same size") unless first_strand.length == second_strand.length
 
     first_strand.each_char.with_index.reject { |c, i| c == second_strand[i] }.length
   end
