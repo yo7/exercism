6e8f523404e53ba27689a0ba15c6ccd5a15d4252
yo7 <bloom6445@gmail.com>
2017-11-25 14:25:02 +0900

    Hamming: Implement compute

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,5 +1,15 @@
+module BookKeeping
+  VERSION = 3
+end
+
 class Hamming
   def self.compute(dna, other)
-    0 if dna == other
+    throw ArgumentError if (dna.length != other.length)
+
+    sum = 0
+    dna.each_char.with_index { |c, i| 
+      sum += 1 if c != other[i]
+    }
+    sum
   end
 end
