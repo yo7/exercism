32b34674760a2f31495d16937a27df762faca1df
yo7 <bloom6445@gmail.com>
2017-11-25 20:23:05 +0900

    Rna Transcription

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class Complement
+  TRANSCRIPTION = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+
+  def self.of_dna(sequence)
+    seq = sequence.chars
+    return '' if seq.any? { |strand| !TRANSCRIPTION.keys.include? strand }
+    seq.map { |strand| TRANSCRIPTION[strand] }.join
+  end
+end
+
+module BookKeeping
+  VERSION = 4
+end
+
+# binding.pry
