e1fe8069ee4369d11f7285f4cb8781d498cacbe3
yo7 <bloom6445@gmail.com>
2017-11-25 15:16:10 +0900

    Hamming: Refactor using count

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -8,7 +8,7 @@ class Hamming
   def self.compute(first_strand, second_strand)
     raise ArgumentError.new("Two strands must be same size") unless first_strand.length == second_strand.length
 
-    first_strand.each_char.with_index.reject { |c, i| c == second_strand[i] }.length
+    first_strand.each_char.with_index.count { |c, i| c != second_strand[i] }
   end
 end
 
