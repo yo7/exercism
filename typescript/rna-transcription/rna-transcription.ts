5f13904d259bc162602e3e8125560f1145fb73a6
yo7 <bloom6445@gmail.com>
2017-11-28 08:16:08 +0900

    TypeScript/Rna Transcript: Refactor

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,16 +1,32 @@
+enum DNA {
+    C = 'C',
+    G = 'G',
+    A = 'A',
+    T = 'T',
+}
+
+const isDNA = (s: string) => Object.keys(DNA).includes(s)
+
+enum RNA {
+    G = 'G',
+    C = 'C',
+    U = 'U',
+    A = 'A',
+}
+
 class Transcriptor {
-    readonly transcript: {[key: string]: string} = {
-        C: 'G',
-        G: 'C',
-        A: 'U',
-        T: 'A'
+    readonly transcript: {[key: string]: RNA} = {
+        [DNA.C]: RNA.G,
+        [DNA.G]: RNA.C,
+        [DNA.A]: RNA.U,
+        [DNA.T]: RNA.A
     }
 
     toRna(s: string): string {
         const nucleotides = s.split('')
 
         for (const strand of nucleotides) {
-            if (!Object.keys(this.transcript).includes(strand)) {
+            if (!isDNA(strand)) {
                 throw new Error('Invalid input DNA.')
             }
         }
