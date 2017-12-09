d576db5f722511671754c13b46e77c79ef6201ec
yo7 <bloom6445@gmail.com>
2017-12-10 00:30:06 +0900

    Go/Space Age: Refactor

--- go/space-age/space_age.go
+++ go/space-age/space_age.go
@@ -3,37 +3,30 @@ package space
 
 type Planet string
 
-const (
-	Earth   Planet = "Earth"
-	Mercury Planet = "Mercury"
-	Venus   Planet = "Venus"
-	Mars    Planet = "Mars"
-	Jupiter Planet = "Jupiter"
-	Saturn  Planet = "Saturn"
-	Uranus  Planet = "Uranus"
-	Neptune Planet = "Neptune"
-)
+var orbitalPeriod = map[Planet]float64{
+	"Earth":   1.0,
+	"Mercury": 0.2408467,
+	"Venus":   0.61519726,
+	"Mars":    1.8808158,
+	"Jupiter": 11.862615,
+	"Saturn":  29.447498,
+	"Uranus":  84.016846,
+	"Neptune": 164.79132,
+}
 
 // Age returns age to pass in with specified seconds in given planet
 func Age(seconds float64, planet Planet) float64 {
-	orbitanPeriod := map[Planet]float64{
-		Earth:   1.0,
-		Mercury: 0.2408467,
-		Venus:   0.61519726,
-		Mars:    1.8808158,
-		Jupiter: 11.862615,
-		Saturn:  29.447498,
-		Uranus:  84.016846,
-		Neptune: 164.79132,
+	period, ok := orbitalPeriod[planet]
+	if !ok {
+		panic("Unknown planet" + planet)
 	}
-
-	return calculateYearBy(seconds, orbitanPeriod[planet])
+	return calculateYear(seconds, period)
 }
 
-func calculateYearBy(seconds float64, f float64) float64 {
+func calculateYear(seconds float64, f float64) float64 {
 	return earthYearsOf(seconds) / f
 }
 
 func earthYearsOf(seconds float64) float64 {
-	return float64(seconds) / (60 * 60 * 24 * 365.25)
+	return seconds / (60 * 60 * 24 * 365.25)
 }
