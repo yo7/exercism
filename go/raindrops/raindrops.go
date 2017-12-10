9abee2fcfae537158a441c57f30e995120676fb3
yo7 <bloom6445@gmail.com>
2017-12-10 11:21:08 +0900

    Go/Raindrops: Refactor

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -2,30 +2,28 @@
 package raindrops
 
 import (
-	"sort"
 	"strconv"
 )
 
-var sounds = map[int]string{
-	3: "Pling",
-	5: "Plang",
-	7: "Plong",
+type rainDrop struct {
+	Factor int
+	Sound  string
+}
+
+var drops = []rainDrop{
+	{Factor: 3, Sound: "Pling"},
+	{Factor: 5, Sound: "Plang"},
+	{Factor: 7, Sound: "Plong"},
 }
 
 // Convert returns matched rain sounds to given number's factors
 func Convert(i int) (s string) {
-	var keys []int
-	for k := range sounds {
-		keys = append(keys, k)
-	}
-	sort.Ints(keys)
-
-	for _, k := range keys {
-		if i%k == 0 {
-			s += sounds[k]
+	for _, drop := range drops {
+		if i%drop.Factor == 0 {
+			s += drop.Sound
 		}
 	}
-	if len(s) == 0 {
+	if s == "" {
 		return strconv.Itoa(i)
 	}
 	return s
