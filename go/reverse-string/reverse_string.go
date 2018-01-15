0f9aca41486b5a0cc8d8663262a1ebef0b248f98
yo7 <bloom6445@gmail.com>
2018-01-16 05:30:19 +0900

    Go: Reverse String

---
+++ go/reverse-string/reverse_string.go
@@ -0,0 +1,13 @@
+// Package reverse implements reversing string
+package reverse
+
+// String reverses given string
+func String(s string) string {
+	n := len(s)
+	runes := make([]rune, n)
+	for _, rune := range s {
+		n--
+		runes[n] = rune
+	}
+	return string(runes[n:])
+}
