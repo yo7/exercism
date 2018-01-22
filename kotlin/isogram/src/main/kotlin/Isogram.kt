1c80243bcf003410bb2837c35d8aecd647772e6e
yo7 <bloom6445@gmail.com>
2018-01-22 18:09:53 +0900

    Kotlin/Isogram: Refactor

--- kotlin/isogram/src/main/kotlin/Isogram.kt
+++ kotlin/isogram/src/main/kotlin/Isogram.kt
@@ -1,8 +1,5 @@
 object Isogram {
-    fun isIsogram(word: String): Boolean {
-        val filtered = word.filterAlphabet()
-        return filtered.all { c -> filtered.count { it == c } <= 1 }
-    }
+    fun isIsogram(word: String) = word.filterAlphabet().groupingBy { it }.eachCount().all { it.value == 1 }
 }
 
 private fun String.filterAlphabet() = this.toLowerCase().replace("[^a-z]".toRegex(), "")
